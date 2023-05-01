aaed26a64781b3e1707f80d000643944cd83aeb3
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-05-01 21:08:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("test");
+function newtask(){
+    const text = document.getElementById("new")
+    const ask = document.getElementById("askingpart").value;
+    const para = document.createElement("label");
+    const broke =document.createElement("br")
+    var checkbox = document.createElement("INPUT");
+    checkbox.type="checkbox"
+    para.innerText = ask;
+    text.appendChild(para);
+    text.appendChild(checkbox);
+    text.appendChild(broke);
+    text.appendChild(broke);
+
+}
